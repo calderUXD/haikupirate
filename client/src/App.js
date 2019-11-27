625850c2cb1940ee41acc0c46da23e98ae0595bb
Rusty Calder <rusty@tinkrlabs.com>
2019-11-26 18:08:08 -0800

    add tracking

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import ReactGA from 'react-ga';
 import styled from 'styled-components';
 //import Nav from "./components/Navigation";
 import Maps from './Maps';
 import Clues from './Clues';
+
+ReactGA.initialize('UA-153423409-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const Body = styled.div`
     width: 100%;
